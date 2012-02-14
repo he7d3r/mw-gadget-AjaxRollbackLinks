0f3189e4c5bdd6e185b4903638760f98c2414821
Helder <he7d3r@gmail.com>
2012-02-14 17:35:53 +0000

    Script to make [rollback] links for edits excecute without an additional page load
    Based on
    * https://pt.wikibooks.org/wiki/meta:User:Krinkle/Scripts/AjaxPatrolLinks.js
    * https://en.wikipedia.org/wiki/User:Gracenotes/rollback.js
    
    Note: this code was originally hosted on Portuguese Wikibooks (https://pt.wikibooks.org)

---
+++ src/AjaxRollbackLinks.js
@@ -0,0 +1,69 @@
+/* Makes [rollback] links for edits excecute without an additional page load. Including self-closing bookmarklet option.
+ * Based on [[meta:User:Krinkle/Scripts/AjaxPatrolLinks.js]] and [[w:en:User:Gracenotes/rollback.js]]
+ * See [[bugzilla:31270]]
+ * FIXME: Use MediaWiki API
+ */
+function ajaxRollback() {
+	var	$rollbackLinks = $('.mw-rollback-link a'),
+		prevUser,
+		rollbackSummaryDefault;
+	function useAJAX(e) {
+		e.preventDefault();
+		var $this = $(this);
+		var href = $this.attr( 'href' ) + '&bot=1';
+		$this.text('Rolling back...');
+		$rollbackLinks = $this.parent();
+		$.get(
+			href,
+			null,
+			function( data, status, request ) {
+				if ( status == 'success' ) {
+					$this.html('<span style="color:green">Rolled back</span>');
+				} else {
+					$this.html('<span style="color:red">Rollback failed</span>');// MediaWiki:Rollbackfailed
+				}
+			}
+		);
+	}
+	if ( $rollbackLinks.length > 0 ) {
+		rollbackSummaryDefault = 'Foram revertidas as edições de $user';
+		prevUser = $('#mw-diff-otitle2').find('a').first().text();
+		if ( prevUser ) {
+			rollbackSummaryDefault += ', com o conteúdo passando a estar como na última edição de ' + prevUser;
+		}
+		rollbackSummaryDefault += '.';
+		$rollbackLinks.each(function(){
+			var $this = $(this);
+			$this.after(
+				$this.clone()
+				.text('+sum')
+				.attr( 'class', '')
+				.click(function confirmRollback( e ) {
+					var	url = this.href,
+						user = url.match( /[?&]from=([^&]*)/ );
+					e.preventDefault();
+					if ( !user ) {
+						return;
+					}
+					user = decodeURIComponent( user[1].replace(/\+/g, ' ') );
+					var extraSum = prompt(
+						'Informe mais detalhes sobre o motivo desta reversão.\n\n' +
+						'Deixe em branco para utilizar o padrão.' +
+						' $user será trocado por "' + user + '".'
+					);
+					if (extraSum === null){
+						return;
+					} else if (extraSum === ''){
+						useAJAX.call(this, e);
+					}
+					this.href += "&summary=" + encodeURIComponent(
+						(rollbackSummaryDefault + ' ' + extraSum).replace( /\$user/g, user )
+					);
+					useAJAX.call(this, e);
+				} )
+			).after( ' | ' )
+			.click( useAJAX );
+		});
+	}
+}
+$(ajaxRollback);
